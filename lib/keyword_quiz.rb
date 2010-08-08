35acd6c3d59804054fcb759037a3c58c7a9c9587
markysoft <markharrop@yahoo.com>
2010-08-08 12:10:42 +0100

    renamed test, updated to be a module

---
+++ lib/keyword_quiz.rb
@@ -0,0 +1,10 @@
+
+require "relative"
+require_relative "keyword_quiz/quiz"
+require_relative "keyword_quiz/question"
+require_relative "keyword_quiz/moviekeywords"
+require_relative "keyword_quiz/movie_info"
+
+module KeywordQuiz
+  VERSION = 0.1
+end
